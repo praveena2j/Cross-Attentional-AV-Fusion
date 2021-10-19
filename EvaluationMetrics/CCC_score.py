ad20afd76cbe1cf038ec758e0d5ef559cdf8ddc8
Gnana Praveen R <47923624+praveena2j@users.noreply.github.com>
2021-10-19 00:36:24 -0400

    Add files via upload

---
+++ EvaluationMetrics/CCC_score.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from torch.autograd import Variable
+import sys
+
+
+def CCC_score(x, y):
+    vx = x - np.mean(x)
+    vy = y - np.mean(y)
+    rho = np.sum(vx * vy) / (np.sqrt(np.sum(vx**2)) * np.sqrt(np.sum(vy**2)))
+    x_m = np.mean(x)
+    y_m = np.mean(y)
+    x_s = np.std(x)
+    y_s = np.std(y)
+    ccc = 2*rho*x_s*y_s/(x_s**2 + y_s**2 + (x_m - y_m)**2)
+    return ccc
